38577a78dc73f336bac9f8c2efb95956649bddea
GIL <gil.00000032610@unicap.br>
2023-02-17 17:40:07 -0300

    Initial commit

---
+++ src/CodingEx38/Ceiling.java
@@ -0,0 +1,21 @@
+package CodingEx38;
+
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+    
+
+}
---
+++ src/CodingEx39/Printer.java
@@ -0,0 +1,13 @@
+package CodingEx39;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+    }
+
+}
