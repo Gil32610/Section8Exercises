c84fccff301553f9676858bfe555e0e7ace19e92
GIL <gil.00000032610@unicap.br>
2023-02-22 15:39:56 -0300

    NOW I GOT THE EX

--- src/CodingEx39/Printer.java
+++ src/CodingEx39/Printer.java
@@ -7,26 +7,27 @@ public class Printer {
 
     public Printer(int tonerLevel, boolean duplex) {
         this.pagesPrinted = 0;
-        this.tonerLevel = (tonerLevel>=0 && tonerLevel<=100)?tonerLevel: -1;
+        this.tonerLevel = (tonerLevel>-1 && tonerLevel<= 100)? tonerLevel: -1;
         this.duplex = duplex;
     }
-
     public int addToner(int tonerAmount){
-        tonerLevel+= tonerAmount;
-        if(tonerLevel> 100 || tonerLevel < 0){
-            return -1;
+        if(tonerAmount<=100 && tonerAmount>0){
+            if(!(this.tonerLevel+tonerAmount>100)){
+                this.tonerLevel+=tonerAmount;
+                return this.tonerLevel;
+            }
+            return-1;
         }
-        return tonerLevel;
+        return-1;
     }
-
     public int printPages(int pages){
-        int jobPages = (duplex)? (pages/2) + (pages%2): pages;
-        pagesPrinted+= jobPages;
-        return jobPages;
+        int pagesToPrint= pages;
+        pagesToPrint = duplex? ((pages/2)+(pages%2)):pages;
+        this.pagesPrinted+= pagesToPrint;
+        return pagesToPrint;
     }
 
     public int getPagesPrinted() {
         return pagesPrinted;
     }
-
 }
